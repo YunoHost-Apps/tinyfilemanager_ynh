7ff6422b58e9dedc97b8617abb29cb1665a18a95
jarod5001 <68397534+jarod5001@users.noreply.github.com>
2022-05-08 17:49:09 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,6 +7,7 @@
 # dependencies used by the app
 # pkg_dependencies="deb1 deb2 php$YNH_DEFAULT_PHP_VERSION-deb1 php$YNH_DEFAULT_PHP_VERSION-deb2"
 YNH_PHP_VERSION="8.0"
+pkg_dependencies="php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-posix php${YNH_PHP_VERSION}-bz2 php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-memcache php${YNH_PHP_VERSION}-memcached php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-sockets php${YNH_PHP_VERSION}-mcrypt php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-fileinfo php${YNH_PHP_VERSION}-mbstring"
 
 #=================================================
 # PERSONAL HELPERS
