3d2b03e0f441332e746de476ad5adf05b8728bbc
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2022-12-19 17:55:26 +0100

    clean scripts

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,9 +5,12 @@
 #=================================================
 
 # dependencies used by the app
-# pkg_dependencies="deb1 deb2 php$YNH_DEFAULT_PHP_VERSION-deb1 php$YNH_DEFAULT_PHP_VERSION-deb2"
+
 YNH_PHP_VERSION="8.0"
-pkg_dependencies="php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-iconv php${YNH_PHP_VERSION}-bz2 php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-fileinfo php${YNH_PHP_VERSION}-mbstring"
+
+php_dependencies="php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-iconv php${YNH_PHP_VERSION}-bz2 php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-fileinfo php${YNH_PHP_VERSION}-mbstring"
+
+pkg_dependencies="$php_dependencies"
 
 #=================================================
 # PERSONAL HELPERS
