7d9a73b33ec720de7327ac3ee0b5ded6182f8875
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-05-01 18:15:43 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,14 +4,6 @@
 # COMMON VARIABLES
 #=================================================
 
-# dependencies used by the app
-
-#REMOVEME? YNH_PHP_VERSION="8.0"
-
-#REMOVEME? php_dependencies="php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-iconv php${YNH_PHP_VERSION}-bz2 php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-fileinfo php${YNH_PHP_VERSION}-mbstring"
-
-#REMOVEME? pkg_dependencies="$php_dependencies"
-
 #=================================================
 # PERSONAL HELPERS
 #=================================================
